444422028b652e79de74097e75ab1bf0f41f4ed0
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2018-04-26 22:26:15 +0100

    remove twitter keys

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
+# Twitter view
+
 ## To install
 run npm i or yarn
+
+then you will need add twitter account information to file src/config.js
+
 
 ## To run
 npm start or yarn start
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'ffiXNHvbCW8QIPoquje1IOqIj',
-    consumer_secret:      '8XNXi8NuHMGUfoVpoLfeONsuk96kCwj02RRtMFwXISlLakPxYd',
-    access_token:         '988534497665839106-92wMavuXox4S2Pf1nP9bc6zMwInoYZC',
-    access_token_secret:  'T17A6ikkk8YUC3brdWGpilNCA87mQyrdMWJ4YXGAOFE2X'
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
 };
